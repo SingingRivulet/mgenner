9177bc2b9a8ed6b3f1626d6c60cc9eb6d3eb78af
cgoxopx <cgoxopx@qq.com>
2019-09-13 17:18:47 +0800

    播放

--- player.js
+++ player.js
@@ -8,9 +8,6 @@ window.onload = function () {
 		},
 		onsuccess: function() {
 			MIDI.setVolume(0, 127);
-			MIDI.noteOn(0, note, velocity, delay);
-			MIDI.noteOff(0, note, delay + 0.75);
-			MIDI.noteOn(0, note, velocity, 3);
 		}
 	});
 };
